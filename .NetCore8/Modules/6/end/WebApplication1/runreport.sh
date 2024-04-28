c4cc0bef0f9c64e9786c62d02dec6c74a0909d56
thimotyb <thimoty@thimoty.it>
2024-04-28 16:14:22 +0200

    Update runreport.sh

--- .NetCore8/Modules/6/end/WebApplication1/runreport.sh
+++ .NetCore8/Modules/6/end/WebApplication1/runreport.sh
@@ -2,36 +2,17 @@
 
 # Funzione per stampare l'uso corretto dello script
 print_usage() {
-    echo "Utilizzo: $0 --name <nome>"
+    echo "Utilizzo: $0 <nome>"
     exit 1
 }
 
-# Verifica se sono stati passati argomenti
+# Verifica se è stato passato almeno un argomento
 if [ $# -eq 0 ]; then
     print_usage
 fi
 
-# Analizza gli argomenti
-while [[ $# -gt 0 ]]; do
-    key="$1"
-
-    case $key in
-        --name)
-            name="$2"
-            shift
-            ;;
-        *)
-            # Ignora argomenti sconosciuti
-            ;;
-    esac
-    shift
-done
-
-# Verifica se il parametro --name è stato specificato
-if [ -z "$name" ]; then
-    echo "Errore: Il parametro --name non è stato specificato."
-    print_usage
-fi
+# Legge il nome dall'argomento
+name="$1"
 
 # Stampa il saluto
 echo "Ciao, $name!"
